e6b5299da48b2018687e6da0f3fa2fb589d9d35b
aman <dixit.aman3@gmail.com>
2025-06-24 19:54:23 +0000

    update az in ec2.tf

--- ec2.tf
+++ ec2.tf
@@ -1,6 +1,7 @@
 resource "aws_instance" "web" {
     ami = "ami-0f918f7e67a3323f0"
     instance_type = "t2.micro"
+    availability_zone = data.aws_availability_zones.available.names[0]
     subnet_id = aws_subnet.public[0].id
     tags = {
         Name = "web-server"
